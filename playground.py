cf6c1bbd1841aa8fe33fffa18abda84b6cc718a1
KhizarAziz <khizar@scanbot.io>
2023-11-04 16:14:35 +0100

    added css selector yaml for centralized handling

--- playground.py
+++ playground.py
@@ -1,81 +1,109 @@
-# import pandas as pd
-# import selenium
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
+import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-from time import sleep
-import undetected_chromedriver as uc
 from fake_useragent import UserAgent
-import random
-
+from time import sleep
+from random import uniform
 import constants
+import yaml
 
 
-# creating options for the browser to
+# Assuming YAML file is named 'element_selectors.yaml' and is in the same directory as your script
+with open('element_selectors.yaml', 'r') as config_file:
+    selectors_config = yaml.safe_load(config_file)
+
+# Function to introduce a random short delay before performing an action
+def wait_with_random_delay(delay_range=(0.5, 2.0)):
+    sleep(uniform(*delay_range))
+
+# creating options for the browser to use
 op = uc.ChromeOptions()
 ua = UserAgent()
-op.add_argument(f"--user-agent={ua.random}")# adding a random agent so that we dont get blocked.
+op.add_argument(f"--user-agent={ua.random}")  # adding a random agent so that we don't get blocked.
 op.add_argument('--no-first-run --no-service-autorun --password-store=basic')
 op.add_argument("--user-data-dir=./website-data-cookies-etc")
-op.add_argument("--headless")
-# Further arguments are listed here: https://peter.sh/experiments/chromium-command-line-switches/
+# op.add_argument("--headless")
 
-driver = uc.Chrome(version_main=118, options=op)
+driver = uc.Chrome(options=op)
 
-MAIL = constants.MAIL
-PASSWORD = constants.PASS
+driver.get(constants.MAIN_URL)
 
-driver.get('https://chat.openai.com/auth/login')
+# Function to perform action based on YAML config
+def perform_action(driver, config):
+    wait = WebDriverWait(driver, 15)
+    selector = config['selector']
+    action_type = config['action']
+    wait_times = config.get('wait_before_action', [0, 0])
+    
+    # Random wait before action
+    sleep(uniform(*wait_times))
 
-sleep(3)
-i = 1
-print('doing ste:',i)
+    try:
+        if action_type == 'clickable':
+            button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, selector)))
+            button.click()
+        elif action_type == 'input_field':
+            print("input value was :",config['input_value'])
+            text_to_input = config['input_value']
+            input_field = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, selector)))
+            input_field.send_keys(text_to_input)
+        
+        elif action_type == 'readable':
+            return wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, selector)))
+        else:
+            raise ValueError(f"Action type '{action_type}' not supported.")
+    except Exception as e:
+        raise Exception(f"An error occurred on selector: {(config.keys())[0]} The error is ",e)
 
-inputElements = driver.find_elements(By.TAG_NAME, "button")
-inputElements[0].click()
 
-sleep(3)
-print('doing ste:',i)
 
-mail = driver.find_elements(By.TAG_NAME,"input")[1]
-mail.send_keys(MAIL)
+'''
+###########################################################################
+                            ON LOGIN PAGE
+###########################################################################
+'''
 
-btn=driver.find_elements(By.TAG_NAME,"button")[0]
-btn.click()
+config_page = selectors_config['login_page']
 
-password= driver.find_elements(By.TAG_NAME,"input")[2]
-password.send_keys(PASSWORD)
+# login
+perform_action(driver, config_page['login_button'])
 
-sleep(3)
-print('doing ste:',i)
+# enter email
+config_page['email_input']['input_value'] = constants.MAIL
+perform_action(driver, config_page['email_input'])
 
-wait = WebDriverWait(driver, 10)
-btn = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "_button-login-password")))
-btn.click()
-sleep(10)
-print('doing ste:',i)
+# click continue
+perform_action(driver, config_page['next_button'])
 
-inputElements = driver.find_elements(By.TAG_NAME, "textarea")
+# enter pass
+config_page['password_input']['input_value'] = constants.PASS
+perform_action(driver, config_page['password_input'])
 
-prompt =  "what is 2+2?"
-inputElements[0].send_keys(prompt)
-sleep(2)
-print('doing ste:',i)
-inputElements[0].send_keys(Keys.ENTER)
-sleep(10)
-print('doing ste:',i)
-inputElements = driver.find_elements(By.TAG_NAME, "p")
-sleep(5)
-print('doing ste:',i)
-results = []
-for element in inputElements:
-    results.append(element.text)
-    print(results)
-sleep(10)
-print('doing ste:',i)
+# finally login
+perform_action(driver, config_page['final_login_button'])
 
-# driver.quit()
+'''
+###########################################################################
+                            ON PROMPT PAGE
+###########################################################################
+'''
+config_page = selectors_config['prompt_page']
+
+# enter text prompt
+config_page['prompt_textarea']['input_value'] = "what is the value of 233 + 67?"
+perform_action(driver, config_page['prompt_textarea'])
+
+
+# sent prompt
+perform_action(driver, config_page['submit_prompt_button'])
+
+# fetch response
+prompt_response_elements = perform_action(driver, config_page['prompt_response_elements'])
+results = [element.text for element in prompt_response_elements]
+print(results)
+
+sleep (20)
+# Continue replacing the rest of the interactions similarly...
+driver.quit()
