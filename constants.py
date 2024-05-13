39eedbed11bd99fa988438fdad87e927f0d5ed2c
KhizarAziz <khizar@scanbot.io>
2024-05-14 01:35:28 +0200

    added default wait time before action

--- constants.py
+++ constants.py
@@ -10,12 +10,13 @@ CHROME_OPTIONS_ARGS = [
     '--password-store=basic',
     '--user-data-dir=./website-data-cookies-etc',
     '--incognito',
-    # '--headless'  # Uncomment if headless mode is desired
+    '--headless'  # Uncomment if headless mode is desired
 ]
+
 
 MAIN_URL="https://chat.openai.com/auth/login"
 LOGIN_METHOD = "login_normal" # login_with_google
 
 BASELINE_PROMPT = "Please keep the response in 100 words!" # dummy example, not used yet
 
-DEFAULT_WAIT_BEFORE_ACTION_TIME = [0.5, 1]
+DEFAULT_WAIT_BEFORE_ACTION_TIME = [1, 3]
