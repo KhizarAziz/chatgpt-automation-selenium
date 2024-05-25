c986418915e1a07b8ea63aa97b99b0dee0bef621
KhizarAziz <khizar@scanbot.io>
2024-05-26 00:04:17 +0200

    code fix

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from auto_gpt import AutoGPT
 
 # i have updated the auto_gpt.py script so this sample might not work as expected...!
 
-MAIN_URL="https://chatgpt.com/"
+MAIN_URL="https://chatgpt.com/?temporary-chat=true"
 
 LOGIN_METHOD = ("login_normal","login_with_google")[0] # choose your option
 
 LOGIN_NEEDED = False
 
 # Example usage:
-auto_gpt = AutoGPT(MAIN_URL, LOGIN_METHOD, LOGIN_NEEDED)
-conversation_list = auto_gpt.enter_prompt("what is the value of 233 + 67?")
+auto_gpt = AutoGPT(MAIN_URL, LOGIN_NEEDED, LOGIN_METHOD)
+conversation_list = auto_gpt.enter_prompt("what is the value of 233 + 67? \n") # make sure to append \n as suffix to submit prompt
 
 print("Conversation list:", conversation_list)
 auto_gpt.quit()
