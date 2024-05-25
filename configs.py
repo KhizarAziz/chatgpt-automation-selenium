99d85058e698d7f2b1a6598e67707dfd528ec2d0
KhizarAziz <khizar@scanbot.io>
2024-05-25 23:30:47 +0200

    paths and creds

--- configs.py
+++ configs.py
@@ -1,3 +1,7 @@
+
+from os import path, getenv
+
+
 CHROME_OPTIONS_ARGS = [
     '--no-first-run',
     '--no-service-autorun',
@@ -17,6 +21,16 @@ CHROME_OPTIONS_ARGS = [
     # '--headless',  # Uncomment if headless mode is desired
 ]
 
-BASELINE_PROMPT = "Please keep the response in 100 words!" # dummy example, not used yet
 
+"""---------------------- PATHS ----------------------"""
+ # Get the project root directory (please note that this wont be root if you using this project as module)
+PROJECT_ROOT = path.abspath(path.dirname(path.abspath(__file__)))
+ELEMENT_SELECTORS_YAML_PATH = path.join(PROJECT_ROOT, 'element_selectors.yaml')
+
+"""---------------------- CREDENTIALS ----------------------"""
+GPT_EMAIL = getenv('GPT_MAIL')
+GPT_PASSWORD = getenv('GPT_PASS')
+
+"""---------------------- APP CONFIGS ----------------------"""
+BASELINE_PROMPT = "Please keep the response in 100 words!" # dummy example, not used yet
 DEFAULT_WAIT_BEFORE_ACTION_TIME = [2, 5]
