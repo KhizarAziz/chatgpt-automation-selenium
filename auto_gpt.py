7ca108671612f06a056de267086e6413ea3cf362
KhizarAziz <khizar@scanbot.io>
2024-05-30 20:34:56 +0200

    updated wait method, name fixes and cleaned code

--- auto_gpt.py
+++ auto_gpt.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from fake_useragent import UserAgent
 from time import sleep
 from random import uniform
-from configs import CHROME_OPTIONS_ARGS, DEFAULT_WAIT_BEFORE_ACTION_TIME, ELEMENT_SELECTORS_YAML_PATH,GPT_EMAIL,GPT_PASSWORD
+from .configs import CHROME_OPTIONS_ARGS, ELEMENT_SELECTORS_YAML_PATH,GPT_EMAIL,GPT_PASSWORD
 import yaml
 from collections import defaultdict
 
@@ -74,46 +74,73 @@ class AutoGPT:
         """Loads YAML configuration from the specified file."""
         with open(file_path, 'r') as config_file:
             return yaml.safe_load(config_file)
-
-    def wait_with_random_delay(self, delay_range=(0.5, 2.0)):
-        """Pauses the program for a random time within the given range to mimic human interaction."""
-        sleep(uniform(*delay_range))
+    def wait_with_random_delay(self, sleep_time):
+        """Pauses the program for a random time within the given range, with a countdown displayed on a single line."""
+        random_wait = uniform(sleep_time, sleep_time + 5)  # Random wait time between sleep_time and sleep_time + 5
+        countdown_time = round(random_wait)
+        while countdown_time > 0:
+            print(f"Wait {countdown_time} seconds", end='\r')
+            sleep(1)
+            countdown_time -= 1
+        print("Done waiting!        ")  # Clear the line after the countdown completes        
 
     # Function to perform action based on YAML config
-    def perform_action(self, config):
-        wait = WebDriverWait(self.driver, 15)
+    def perform_action(self, config, explicit_wait_before_action=None):
+        wait = WebDriverWait(self.driver, 10)
         selector = config['selector']
+        selector_type = config['selector_type']
         action_type = config['action']
-        wait_time_s = config.get('wait_before_action', DEFAULT_WAIT_BEFORE_ACTION_TIME)
-        self.wait_with_random_delay(wait_time_s) # Random wait before action
+        # overwriting the default wait time from yaml if there is explicit wait time passed to method.
+        wait_time_s = explicit_wait_before_action if explicit_wait_before_action is not None else config.get('default_wait_before_action', 0)
 
-        print(f"Doing ActionType: {action_type}  -  Selector {selector} , Waiting {wait_time_s} seconds")
+        print(f"Action_Type {action_type} -  Selector: {selector} - Wait_Time: {wait_time_s}")
+        self.wait_with_random_delay(wait_time_s)
+
         try:
-            if action_type == 'clickable':
+            if action_type == 'click':
                 if selector.startswith("//"):  # Indicates an XPath selector
                     button = wait.until(EC.element_to_be_clickable((By.XPATH, selector)))
                 else:  # Defaults to CSS Selector
                     button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, selector)))
                 button.click()
-            elif action_type == 'input_field':
+            elif action_type == 'input_text':
                 text_to_input = config['input_value']
                 if selector.startswith("//"):  # Indicates an XPath selector
                     input_field = wait.until(EC.visibility_of_element_located((By.XPATH, selector)))
                 else:  # Defaults to CSS Selector
                     input_field = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, selector)))
                 input_field.send_keys(text_to_input)
-            elif action_type == 'readable':        
+            elif action_type == 'extract_conversation':
+                # first wait then fetch : Let the text fully generate first 
                 if selector.startswith("//"):  # Indicates an XPath selector
                     return wait.until(EC.visibility_of_all_elements_located((By.XPATH, selector)))
                 else:  # Defaults to CSS Selector
                     return wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, selector)))
             else:
-                raise ValueError(f"Action type '{action_type}' not supported.")
+                raise ValueError(f"action_type {action_type} not found...! For Selector: {selector}, selector_type: '{selector_type}' ")
         except Exception as e:
-            error_message = str(e)  # Replace this with the actual error message
-            print(f"Action Failed on selector {selector}  with error {error_message}")
-            # raise ValueError()
-            exit()
+            raise ValueError(f"Action Failed on selector {selector}  with error {str(e)}")
+
+    def enter_prompt(self, prompt_text, start_new_chat=False, wait_before_response_fetch=None) -> defaultdict:
+        """Submits a text prompt on a page and returns the response as text."""
+        if start_new_chat:
+            self.refresh_page_to_start_new_chat()
+        prompt_page = self.selectors_config['prompt_page']
+        prompt_page['prompt_input_text_field']['input_value'] = prompt_text
+        self.perform_action(prompt_page['prompt_input_text_field']) # sending prompt
+        
+        whole_conversation = self.perform_action(prompt_page['conversation_listview'],explicit_wait_before_action = wait_before_response_fetch ) # fetching whole chat
+        last_message_element = whole_conversation[-1] # reading last reponse from gpt
+        
+        message_dict = defaultdict()
+        message_dict['text'] = last_message_element.text # last message in the gpt chat
+        
+        
+        image_elements = last_message_element.find_elements(By.TAG_NAME, 'img') # Locate all images in last_message_element
+        if len(image_elements) > 0:
+            print("Images found: ",len(image_elements))
+            message_dict['img_url'] = image_elements[0].get_attribute('src') # getting the last image's source
+        return message_dict
 
     def login(self, login_type) -> None:
         """Logs into the website using predefined YAML configurations for user interactions."""
@@ -143,36 +170,11 @@ class AutoGPT:
 
     def refresh_page_to_start_new_chat(self):
         # Refresh the page
-        print("refersing page")
+        print("Refershing page")
         self.driver.refresh()
 
     
-    def enter_prompt(self, prompt_text,start_new_chat=False) -> defaultdict:
-        """Submits a text prompt on a page and returns the response as text."""
-        if start_new_chat:
-            self.refresh_page_to_start_new_chat()
-        config_page = self.selectors_config['prompt_page']
-        config_page['prompt_input_text_field']['input_value'] = prompt_text
-        self.perform_action(config_page['prompt_input_text_field'])
-        # self.perform_action(config_page['submit_prompt_button']) # prompt submit button is no more needed!
-        # results = [element.text for element in whole_conversation] # extracting values from each of chat bubble
-        whole_conversation = self.perform_action(config_page['conversation_listview'])
-        last_message_element = whole_conversation[-1]
-        
-        
-        message_dict = defaultdict()
-        message_dict['text'] = last_message_element.text # last message in the gpt chat
-        
-        
-        image_elements = last_message_element.find_elements(By.TAG_NAME, 'img') # Locate all images in last_message_element
-        if len(image_elements) > 0:
-            message_dict['img_url'] = image_elements[-1].get_attribute('src') # getting the last image's source
-            print("Images found: ",message_dict['img_url'])
-        else:
-            message_dict['img_url'] = None
-        return message_dict
-    
     def quit(self):
         """Quit Driver upon action done!"""
-        self.wait_with_random_delay()
+        self.wait_with_random_delay(5)
         self.driver.quit()
